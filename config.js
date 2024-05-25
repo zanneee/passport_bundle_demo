7a27fed7dde437f73f92cb3c5ad5add2b8d50d72
Jinsung Kim <zanneee@gmail.com>
2024-05-25 14:12:15 +0900

    added passport env

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 const env = require("dotenv").config();
 
 module.exports = {
-  TESTNET: {            
+  TESTNET: {
+    env: process.env.IMTBL_ENVIROMENT,
     client_id: process.env.IMTBL_TESTNET_PASSPORT_CLIENT_ID,
     redirect_url: process.env.IMTBL_TESTNET_PASSPORT_LOGIN_REDIRECT_URL,
     logout_url: process.env.IMTBL_TESTNET_PASSPORT_LOGOUT_REDIRECT_URL,
@@ -10,6 +11,7 @@ module.exports = {
     base_url: process.env.IMTBL_TESTNET_BASE_URL
   },
   MAINNET: {        
+    env: process.env.IMTBL_ENVIROMENT,
     client_id: process.env.IMTBL_MAINNET_PASSPORT_CLIENT_ID,
     redirect_url: process.env.IMTBL_MAINNET_PASSPORT_LOGIN_REDIRECT_URL,
     logout_url: process.env.IMTBL_MAINNET_PASSPORT_LOGOUT_REDIRECT_URL,
